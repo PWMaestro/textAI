76b6677dab5534a3f78b999cb3068a1a334b7790
Artiom Sitko <AC3600@yandex.by>
2021-04-21 11:52:23 +0300

    Clean code in compareStrings function

--- textAI.cpp
+++ textAI.cpp
@@ -218,11 +218,11 @@ int compareStrings(const string &str1, const string &str2)
 
     for (int i = 0; i < maxLength; i++)
     {
-        if ( str1[i] > str2[i] )
+        if (str1[i] > str2[i])
         {
             return 1;
         }
-        else if ( str1[i] < str2[i] )
+        else if (str1[i] < str2[i])
         {
             return -1;
         }
